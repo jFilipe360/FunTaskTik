9f5721720122e4bd47da2d70df76882572cfc7ac
Joao Correia <aluno21302@ipt.pt>
2023-12-20 16:53:54 +0000

    Fix - targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.dam.funtasktik"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dam.funtasktik"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
